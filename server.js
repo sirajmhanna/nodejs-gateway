6e2e6c02584ca9fc0544ab87a0dd026d2102230b
Siraj <59828433+sirajmhanna@users.noreply.github.com>
2021-10-31 21:44:44 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 5000;
+const logger = require('./config/winston');
+
+const server = http.createServer(app);
+server.listen(port, () => {
+    logger.info(new Date().getTime(), 'server', 'listen', `Server is up`, { port });
+});
