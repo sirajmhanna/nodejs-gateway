800646d5f3aa920dbcc5509d938b7d1f30e2612f
Siraj <sirajmhanna@hotmail.com>
2021-11-03 12:36:02 +0200

    Create common-responses.js

---
+++ helpers/common-responses.js
@@ -0,0 +1,12 @@
+module.exports = {
+    genericServerError: {
+        status: 'fail',
+        code: 500,
+        message: 'serverError'
+    },
+    somethingWentWrong: {
+        status: 'fail',
+        code: 400,
+        message: 'somethingWentWrong'
+    }
+};
