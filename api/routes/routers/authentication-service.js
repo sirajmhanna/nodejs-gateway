10ebdeae3b64de01f0af8d8edbdd5817ddb11f71
Siraj <sirajmhanna@hotmail.com>
2021-11-03 14:40:11 +0200

    Update authentication-service.js

--- api/routes/routers/authentication-service.js
+++ api/routes/routers/authentication-service.js
@@ -2,11 +2,19 @@ const express = require('express');
 const route = express.Router();
 const RequestMiddlewares = require('../../middlewares/request');
 const AuthenticationService = require('../../controllers/routers/authentication-service');
+const AuthenticationMiddleware = require('../../middlewares/authentication');
 
 // Login Route
 route.post('/api/authentication/login',
     RequestMiddlewares.generateRequestIdentifier,
     AuthenticationService.login
+);
+
+// Logout Route
+route.post('/api/authentication/logout',
+    RequestMiddlewares.generateRequestIdentifier,
+    AuthenticationMiddleware.isAuthenticated,
+    AuthenticationService.logout
 );
 
 module.exports = route;
