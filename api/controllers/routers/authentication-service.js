cace02a8b0c39da4cd99fbe34be0774ab62ff060
Siraj <sirajmhanna@hotmail.com>
2021-11-03 14:40:06 +0200

    Update authentication-service.js

--- api/controllers/routers/authentication-service.js
+++ api/controllers/routers/authentication-service.js
@@ -21,7 +21,7 @@ exports.login = async (req, res) => {
 
         if(response.status === 'success'){
             logger.info(req.body.requestID, 'authentication-service', 'login', 'Set refresh token in httpOnly secured cookie', {});
-            res.cookie("refreshToken", response.refreshToken, {
+            res.cookie("refreshToken", response.refreshToken.refresh, {
                 maxAge: ((1000) * response.refreshToken.time),
                 httpOnly: true,
                 sameSite: !process.env.ENVIRONMENT === "development",
@@ -34,6 +34,32 @@ exports.login = async (req, res) => {
         return res.status(response.code).json(response);
     } catch (error) {
         logger.error(req.requestID, 'authentication-service', 'login', 'Server Error', { error: error.toString() });
+        return res.status(500).json(commonResponses.genericServerError);
+    }
+};
+
+// Logout Router
+exports.logout = async (req, res) => {
+    try {
+        logger.info(req.requestID, 'authentication-service', 'logout', 'Starting execution :: Calling authentication service', { path: req.path, ipAddress: req.ip });
+        let response = await nodeFetch(process.env.AUTHENTICATION_SERVICE_URL + req.path,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    requestID: req.requestID,
+                    refreshToken: req.cookies['refreshToken']
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': req.headers.authorization
+                }
+            });
+        response = await response.json();
+
+        logger.info(req.requestID, 'authentication-service', 'logout', 'Returning response', { ipAddress: req.ip });
+        return res.status(response.code).json(response);
+    } catch (error) {
+        logger.error(req.requestID, 'authentication-service', 'logout', 'Server Error', { error: error.toString() });
         return res.status(500).json(commonResponses.genericServerError);
     }
 };
