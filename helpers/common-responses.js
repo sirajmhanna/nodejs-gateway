a5231caea59b3176cae9b8f34e1bd5a64819f342
Siraj <sirajmhanna@hotmail.com>
2021-11-03 14:40:13 +0200

    Update common-responses.js

--- helpers/common-responses.js
+++ helpers/common-responses.js
@@ -8,5 +8,10 @@ module.exports = {
         status: 'fail',
         code: 400,
         message: 'somethingWentWrong'
+    },
+    unauthorized: {
+        status: 'fail',
+        code: 401,
+        message: 'unauthorized'
     }
 };
