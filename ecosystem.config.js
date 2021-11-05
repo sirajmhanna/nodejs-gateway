1d50e3706352b036f297e6c9963da04efc1a88b6
Siraj <sirajmhanna@hotmail.com>
2021-11-05 12:55:25 +0200

    [FEAT]: ecosystem configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            name: `${process.env.ENVIRONMENT}-gateway`,
+            script: "./server.js",
+            watch: false,
+            env: {
+                PORT: `${process.env.PORT}`,
+                SERVICE_NAME: `${process.env.SERVICE_NAME}`,
+                ENVIRONMENT: `${process.env.ENVIRONMENT}`,
+                AUTHENTICATION_SERVICE_URL: `${process.env.AUTHENTICATION_SERVICE_URL}`,
+                FRONTEND_BASE_URL: `${process.env.FRONTEND_BASE_URL}`
+            },
+        }
+    ]
+};
